13d6a4fb780891800e951eee0ccb77e68dc5c70c
KH <dbstjsdud357@naver.com>
2020-02-11 14:02:17 +0900

    서비스 테스트

---
+++ team_test/src/main/java/com/kh/team_test/service/test_service.java
@@ -0,0 +1,10 @@
+package com.kh.team_test.service;
+
+public class test_service {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			System.out.println("서비스 테스트");
+	}
+
+}
