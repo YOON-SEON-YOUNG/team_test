83a7d64b4c9b127bc0cfc73b4a7dde54db604d4b
Jin <gudwls0645@naver.com>
2020-02-11 12:26:18 +0900

    conflict

--- team_test/src/main/java/com/kh/team_test/HjController.java
+++ team_test/src/main/java/com/kh/team_test/HjController.java
@@ -3,5 +3,6 @@ package com.kh.team_test;
 public class HjController {
 	public static void main(String[] args) {
 		System.out.println("Hi (Jin)");
+		System.out.println("Hi conflict (Jin)");
 	}
 }
