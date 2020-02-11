7025dbc854e00ddac4ae56fc50fbd3c9e9c125e2
KH <dbstjsdud357@naver.com>
2020-02-11 12:43:12 +0900

    테스트

---
+++ team_test/java/com/kh/team_test/HjController.java
@@ -0,0 +1,9 @@
+package com.kh.team_test;
+
+public class HjController {
+	public static void main(String[] args) {
+		System.out.println("Hi (Jin)");
+		System.out.println("Oh~ hi hi (yoon)");
+		
+	}
+}
